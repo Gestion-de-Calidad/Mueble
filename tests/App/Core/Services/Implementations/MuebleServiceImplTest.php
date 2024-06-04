7717fff871f25ff012162ae26a4cd0c1ccf1a47f
VisaAF <farinolasantiago@outlook.com>
2024-06-04 19:07:58 -0300

    Se actualizan los metodos del muebleServices

--- tests/App/Core/Services/Implementations/MuebleServiceImplTest.php
+++ tests/App/Core/Services/Implementations/MuebleServiceImplTest.php
@@ -16,4 +16,32 @@ class MuebleServiceImplTest extends TestCase
         $this->assertInstanceOf(Mueble::class, $mueble);
     }
 
+    public function testGetMuebleAll()
+    {
+        $service = new MuebleServiceImpl();
+        $mueble = $service->getAllMuebles();
+        $this->assertIsArray($mueble);
+    }
+
+    public function testInsertMueble()
+    {
+        $service = new MuebleServiceImpl();
+
+        $result = $service->insertMueble();
+        $this->assertPostConditions(true, $result);
+    }
+
+    public function testUpdateMueble()
+    {
+        $service = new MuebleServiceImpl();
+        $result = $service->updateMueble();
+        $this->assertPostConditions(true, $result);
+    }
+
+    public function testDeleteMueble()
+    {
+        $service = new MuebleServiceImpl();
+        $result = $service->deleteMueble(4);
+        $this->assertNotFalse($result);
+    }
 }
