ed358fb64eed64ddbb2816ae9983e2a3503b1339
VisaAF <farinolasantiago@outlook.com>
2024-06-04 00:06:03 -0300

    Se actualiza clase MuebleServiceImplTest

--- tests/App/Core/Services/Implementations/MuebleServiceImplTest.php
+++ tests/App/Core/Services/Implementations/MuebleServiceImplTest.php
@@ -3,9 +3,17 @@
 namespace services;
 
 use App\Core\Services\Implementations\MuebleServiceImpl;
+use App\Core\Models\Mueble;
 use PHPUnit\Framework\TestCase;
 
 class MuebleServiceImplTest extends TestCase
 {
+
+    public function testGetMuebleById()
+    {
+        $service = new MuebleServiceImpl();
+        $mueble = $service->getMuebleById(1);
+        $this->assertInstanceOf(Mueble::class, $mueble);
+    }
 
 }
