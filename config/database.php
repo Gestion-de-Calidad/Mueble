12861ea13e8d716b6ca4a532f4465e63ca6dcab2
VisaAF <farinolasantiago@outlook.com>
2024-06-03 23:14:22 -0300

    Se modifica base de datos de prueba por real

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 namespace config;
 
 return [
-    'dsn' => 'sqlsrv:Server=127.0.0.1,1433;Database=voluntarios',
+    'dsn' => 'sqlsrv:Server=127.0.0.1,1433;Database=mueble',
     'usuario' => 'sa',
     'contraseña' => 'Ss15506358',
 ];
